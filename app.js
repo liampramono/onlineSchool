3a8d53c8f9ed01603c9edd58ab8fb40151734c4f
pramono wang <liampramono@gmail.com>
2018-07-19 00:07:49 -0700

    adding git ignore and connecting database

--- app.js
+++ app.js
@@ -2,21 +2,42 @@ const express = require('express');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const mongo = require('mongodb');
+const mongodb = require('mongodb');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 4001;
+const config = require('./config/secret');
+
 const classes = require('./routes/classes');
+
 
 // const routes = require('/routes/index');
 // const users = require('/routes/user');
-
-const app = express();
-app.use('/', classes);
-
 
 
 // app.use('/', routes);
 // app.use('/users', users);
 // app.use('/classes', classes);
 
-app.listen(port, () => console.log(`Listening on the post ${port}`));
+const app = express();
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(config.mongodb, (err, database) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    console.log(config.mongodb);
+    // Save database object from the callback for reuse.
+    const db = database;
+    console.log("Database connection ready");
+
+
+    app.use('/', classes );
+  
+    // Initialize the app.
+    app.listen(port, () => console.log(`Listening on the post ${port}`));
+    
+  });
+
+
+
